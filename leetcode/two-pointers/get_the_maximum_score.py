4e8d41e51904e31da2f09fdfc16e8a44a0a9c955
Yash Anand <yashanand1910@gmail.com>
2022-05-25 18:26:47 +0530

    Solution(s) added on Wed May 25 18:26:47 IST 2022

---
+++ leetcode/two-pointers/get_the_maximum_score.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSum(self, nums1: List[int], nums2: List[int]) -> int:
+        x, y = 0, 0
+        n1, n2 = len(nums1), len(nums2)
+        total, sum1, sum2 = 0, 0, 0
+        while x < n1 or y < n2:
+            while y < n2 and x < n1 and nums1[x] > nums2[y]:
+                sum2 += nums2[y]
+                y += 1
+            while x < n1 and y < n2 and nums1[x] < nums2[y]:
+                sum2 += nums1[x]
+                x += 1
+            if nums1[x] == nums2[y]:
+                total += max(sum1, sum2)
+                sum1, sum2 = 0, 0
+                sum1 += nums1[x]
+                sum2 += nums2[y]
+                x += 1
+                y += 1
+        total +=  
+        return total
+
