826fd69f23b382b42101c0fbf219b9fb5b8262dd
Yash Anand <yashanand1910@gmail.com>
2022-05-25 21:13:55 +0530

    Solution(s) added on Wed May 25 21:13:55 IST 2022

--- leetcode/two-pointers/get_the_maximum_score.py
+++ leetcode/two-pointers/get_the_maximum_score.py
@@ -4,19 +4,20 @@ class Solution:
         n1, n2 = len(nums1), len(nums2)
         total, sum1, sum2 = 0, 0, 0
         while x < n1 or y < n2:
-            while y < n2 and x < n1 and nums1[x] > nums2[y]:
-                sum2 += nums2[y]
+            while y < n2 and (x == n1 or nums1[x] > nums2[y]):
+                sum1 += nums2[y]
                 y += 1
-            while x < n1 and y < n2 and nums1[x] < nums2[y]:
+            while x < n1 and (y == n2 or nums1[x] < nums2[y]):
                 sum2 += nums1[x]
                 x += 1
-            if nums1[x] == nums2[y]:
-                total += max(sum1, sum2)
-                sum1, sum2 = 0, 0
+            if x < n1 and y < n2 and nums1[x] == nums2[y]:
                 sum1 += nums1[x]
                 sum2 += nums2[y]
+                total += max(sum1, sum2)
+                sum1, sum2 = 0, 0
                 x += 1
                 y += 1
-        total +=  
-        return total
+        total += max(sum1, sum2)
+
+        return total % (10**9 + 7)
 
